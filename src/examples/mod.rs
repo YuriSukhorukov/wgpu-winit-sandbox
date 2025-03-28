f0ea3b71bdbe6377a9aa1c1e1cce03d10334803d
Yuri Sukhorukov <yuri.sukhorukov.spb@gmail.com>
2025-03-28 17:21:46 +0300

    camera

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,3 +1,4 @@
 pub mod triangle;
 pub mod buffers_and_indexes;
 pub mod textures_and_bind_groups;
+pub mod perspective_camera;
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod examples;
 fn main() {
     // examples::triangle::run();
     // examples::buffers_and_indexes::run();
-    examples::textures_and_bind_groups::run();
+    // examples::textures_and_bind_groups::run();
+    examples::perspective_camera::run();
 }
